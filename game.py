7e12ca8281da1f5d5056233519cee31dbb382a24
Siya2704 <68345107+Siya2704@users.noreply.github.com>
2021-11-07 19:19:04 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,19 +1,22 @@
 import pygame
- 
+import sys,time
+sys.setrecursionlimit(100000)
+INF = 1e6
 pygame.init()
 screen = pygame.display.set_mode((1300,900))
 color = (255,204,153)
 screen.fill(color)
 pygame.display.set_caption('Bagh-Chal')
-done = False
 
 img_board = pygame.image.load('img/board.png')
 img_tiger = pygame.image.load('img/tiger.png')
 img_tiger = pygame.transform.scale(img_tiger, (60,60))
 img_goat = pygame.image.load('img/goat.png')
 img_goat = pygame.transform.scale(img_goat, (60,60))
+myfont = pygame.font.SysFont("Comic Sans MS", 50)
 
 def get_coord():
+	global coord
 	coord = [[(170,170),(295,170),(420,170),(545,170),(670,170)],[(170,295),(295,295),(420,295),(545,295),(670,295)],[(170,420),(295,420),(420,420),(545,420),(670,420)],[(170,545),(295,545),(420,545),(545,545),(670,545)],[(170,670),(295,670),(420,670),(545,670),(670,670)]] #2d array
 	#left, right, top, down
 	global occupied
@@ -21,7 +24,7 @@ def get_coord():
 
 	return coord,occupied
 
-def moves(cur_pos,coord):
+def moves(cur_pos,coord,kill):
 	x = cur_pos[0];y = cur_pos[1]
 	#left, right, up, down
 	global pos
@@ -31,6 +34,7 @@ def moves(cur_pos,coord):
 	co1 = [(0,-1),(0,1),(-1,0),(1,0),(-1,-1),(1,-1),(-1,1),(1,1)]
 	co2 = [(0,-1),(0,1),(-1,0),(1,0)]
 	pos_n = []
+	pos_t = []
 	c = coord[x][y]
 	if((c[0]+c[1]) % 2 == 0):
 		pos = pos1
@@ -49,14 +53,17 @@ def moves(cur_pos,coord):
 		if(p >= 0 and q >= 0 and p < 5 and q < 5 and occupied[p][q] == '-'):
 			xn = c[0] + pos[i][0];yn = c[1] + pos[i][1]
 			pos_n.append((xn,yn,i))
+			pos_t.append((p,q))
 		elif(p >= 0 and q >= 0 and p < 5 and q < 5 and occupied[p][q] == 'G'):
 			p1 = p+co[i][0]
 			q1 = q+co[i][1]
 			if(p1 >= 0 and q1 >= 0 and p1 < 5 and q1 < 5 and occupied[p1][q1] == '-'):
 				xn = c[0] + pos[i][0]*2;yn = c[1] + pos[i][1]*2
 				pos_n.append((xn,yn,i))
+				pos_t.append((p1,q1))
+				kill = kill + 1
 	#print(pos_n)
-	return pos_n
+	return pos_n,pos_t,kill
 	
 
 def get_mouse_click(coord, occupied):
@@ -71,9 +78,22 @@ def get_mouse_click(coord, occupied):
 	return (-1,-1)
 
 
-def board(screen,occupied,coord):
+def board(screen,occupied,coord,score,goat_killed):
 	pygame.draw.rect(screen, (255,255,255), pygame.Rect(90, 90, 1120, 720))
 	screen.blit(img_board,(200,200))#size 500X500
+	label = myfont.render("Score:", 1, (0,0,0))
+	screen.blit(label, (800, 200))
+	sc = myfont.render(str(score), 1, (0,0,0))
+	screen.blit(sc, (920, 200))
+	label = myfont.render("Goats Killed:", 1, (0,0,0))
+	screen.blit(label, (800, 250))
+	sc = myfont.render(str(goat_killed), 1, (0,0,0))
+	screen.blit(sc, (1030, 250))
+	
+	label = myfont.render("Goats Remaining:", 1, (0,0,0))
+	screen.blit(label, (800, 300))
+	sc = myfont.render(str(20-goat_killed), 1, (0,0,0))
+	screen.blit(sc, (1110, 300))
 	for i in range(5):
 		for j in range(5):
 			if(occupied[i][j] == 'T'):
@@ -81,74 +101,120 @@ def board(screen,occupied,coord):
 			if(occupied[i][j] == 'G'):
 				screen.blit(img_goat,coord[i][j])
 
+#all possible moves of all tiger
+def all_tiger_moves(arr):
+	pos_tiger = []
+	count = 0
+	for i in range(5):
+		for j in range(5):
+			if(arr[i][j] == 'T'):
+				m = moves((i,j),coord,0)
+				kill = m[2]
+				pos_tiger.append((i,j,kill))
+	return pos_tiger
+
 def goal(kill):
 	return kill == 5
 
-def minimax (curDepth, nodeIndex,maxTurn, scores,targetDepth):
- 
-    # base case : targetDepth reached
-    if (curDepth == targetDepth):
-        return scores[nodeIndex]
-     
-    if (maxTurn):
-        return max(minimax(curDepth + 1, nodeIndex * 2,
-                    False, scores, targetDepth),
-                   minimax(curDepth + 1, nodeIndex * 2 + 1,
-                    False, scores, targetDepth))
-     
-    else:
-        return min(minimax(curDepth + 1, nodeIndex * 2,True, scores, targetDepth), minimax(curDepth + 1, nodeIndex * 2 + 1,True, scores,targetDepth))
-   
-
-def place_goat(occ):
+def isMoveLeft(arr):
 	for i in range(5):
 		for j in range(5):
-			if(occ[i][j] == '-'):
-				if(i == 0 and j == 0):#left top
-					if (occ[i+1][j] == 'T' or occ[i][j+1] == 'T' or occ[i+1][j+1]):
-						occ[i][j] = 'G'
-						break
-					elif((occ[i+1][j] == 'G' and occ[i+2][j] == 'T') or (occ[i][j+1] == 'G' and occ[i][j+2] == 'T')):
-						occ[i][j] = 'G'
-						break
-				elif(i == 4 and j == 0):#right top
-					if (occ[i-1][j] == 'T' or occ[i][j+1] == 'T' or occ[i-1][j+1]):
-						occ[i][j] = 'G'
-						break
-					elif((occ[i-1][j] == 'G' and occ[i-2][j] == 'T') or (occ[i][j+1] == 'G' and occ[i][j+2] == 'T')):
-						occ[i][j] = 'G'
-						break
-				elif(i == 0 and j ==4):#left bottom
-					if (occ[i+1][j] == 'T' or occ[i][j-1] == 'T' or occ[i+1][j-1]):
-						occ[i][j] = 'G'
-						break
-					elif((occ[i+1][j] == 'G' and occ[i+2][j] == 'T') or (occ[i][j-1] == 'G' and occ[i][j-2] == 'T')):
-						occ[i][j] = 'G'
-						break
-				elif(i == 4 and j == 4):#right bottom
-					if (occ[i-1][j] == 'T' or occ[i][j-1] == 'T' or occ[i-1][j-1]):
-						occ[i][j] = 'G'
-						break
-					elif((occ[i-1][j] == 'G' and occ[i-2][j] == 'T') or (occ[i][j-1] == 'G' and occ[i][j-2] == 'T')):
-						occ[i][j] = 'G'
-						break
+			if(arr[i][j] == '-'):
+				return True
+	return False
+	
+#it will return all movable tigers
+def movable_tiger(arr):
+	Tiger = []
+	m_T = 0
+	for i in range(5):
+		for j in range(5):
+			if(arr[i][j] == 'T'):
+				Tiger.append((i,j))
+	for i in Tiger:
+		move = moves(i,coord,0)
+		if(len(move[0]) > 0):
+			m_T = m_T + 1
+	return m_T
+	
+#this will return minimum score for min(goat)
+def minimax(arr,depth,isMax) :
+	if(depth == 9):
+		return 0
+
+	if (isMoveLeft(arr) == False) :
+		return 0
+
+	if (isMax) :
+		#tiger
+		best = 0
+		pos_tiger = all_tiger_moves(arr)
+		print(pos_tiger)
+		for i in pos_tiger:
+			kill = i[2]
+			best = max(best,kill)
+		best = max(best+movable_tiger(arr),minimax(arr,depth+1,not isMax))  #+minimize its blocking
+		print("1st", best)
+		return best
+
+	else :#goat
+		best =  100
+		best = min(best,minimax(arr,depth+1,not isMax)+movable_tiger(arr))  #+maximize its blocking
+		print("2nd", best)
+		return best
 		
-     
+#it will chooose bestMove and return
+def findBestMove(arr,kill) :
+	bestVal = 1000
+	bestMove = (-1, -1)
+	for i in range(5):
+		for j in range(5):
+			if(arr[i][j] == '-'):
+				occupied[i][j] = 'G'
+				moveVal = minimax(arr, 0, False)
+				if(moveVal < bestVal):
+					bestVal = moveVal
+					bestMove = (i,j)
+				occupied[i][j] = '-'	
+				
+	print("The value of the best Move is :", moveVal)
+	print()
+	return bestMove	
+
+		  
 def solve():
 	coord,occupied = get_coord()
 	occupied[0][4] = 'T';occupied[4][0] = 'T';
 	occupied[0][0]  ='T';occupied[4][4] = 'T';
-	occupied[1][0] = 'G'
+	done = False
 	kill = 0
-	flag = 0
+	flag = 1
 	goat_remaining = 20
 	done = False
+	global score
+	score = 0
 	while not done:
-		board(screen,occupied,coord)
+		time.sleep(0.05)
+		board(screen,occupied,coord,score,kill)
 		if(goal(kill)):
-			return True
+			myfont = pygame.font.SysFont("Comic Sans MS", 150)
+			label = myfont.render("You Won!!", 1, (255,0,0))
+			screen.blit(label, (710, 400))
+			done = True
+			pygame.display.flip()
+			time.sleep(5)
+		elif(movable_tiger(occupied) == 0):
+			myfont = pygame.font.SysFont("Comic Sans MS", 150)
+			label = myfont.render("You Lost!!", 1, (255,0,0))
+			screen.blit(label, (710, 400))
+			done = True
+			pygame.display.flip()
+			time.sleep(5)
 		if(flag == 0):
-			place_goat(occupied)
+			#print("Goat")
+			bestMove = findBestMove(occupied,kill)
+			#print(bestMove)
+			occupied[bestMove[0]][bestMove[1]] = 'G'
 			flag = 1
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -163,17 +229,21 @@ def solve():
 					pass
 				else:
 					a1 = coord[cu[0]][cu[1]]
-					move = moves(cd, coord)
-					#print(move)
-					for i in range(len(move)):
-						a3 = move[i]
+					move = moves(cd, coord,kill)
+					a2 = coord[cd[0]][cd[1]]
+					#print("Tiger")
+					for i in range(len(move[0])):
+						a3 = move[0][i]
 						if(a1[0]==a3[0] and a1[1]==a3[1]):
-							if(125 <abs(a1[0] - cd[0])<250  or 125<abs(a1[1]-cd[0])<250):
-								kill = kill + 1
-								#print(co[a3[2]])
-								p = cd[0]+co[a3[2]][0]
-								q = cd[1]+co[a3[2]][1]
-								occupied[p][q] = '-'
+							if(125 <=abs(a1[0] - a2[0])<=250  or 125<=abs(a1[1]-a2[1])<=250):
+								if(abs(a1[0] - a2[0]) == 250  or abs(a1[1]-a2[1]) == 250):#kill goat
+									kill = kill + 1
+									goat_remaining -= 1
+									score += 100
+									#print(co[a3[2]])
+									p = cd[0]+co[a3[2]][0]
+									q = cd[1]+co[a3[2]][1]
+									occupied[p][q] = '-'
 							occupied[cu[0]][cu[1]] = 'T';
 							occupied[cd[0]][cd[1]] = '-';
 							flag = 0#next computer's move
